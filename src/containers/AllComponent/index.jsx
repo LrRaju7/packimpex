de0f83950eb16b01a9ffe9c28f5d25c8ac3c821f
lutforraju <lutfor.raju@sjinnovation.com>
2023-01-03 19:07:58 +0600

    Added static componentID

--- src/containers/AllComponent/index.jsx
+++ src/containers/AllComponent/index.jsx
@@ -10,8 +10,8 @@ const AllComponent = () => {
   return (
     <>
       <Banner componentID='7899f8a5-94a3-48f0-96eb-ef58a57c8b41'/>
-      <TitleDescription />
-      <ImageTextLeftRight />
+      <TitleDescription componentID='781d799b-0133-4f2f-93f8-f54ff6b1788c'/>
+      <ImageTextLeftRight omponentID='0fce1258-91cb-4f12-99df-e2cee6611d29'/>
       <ParagraphTwo />
       <ParagraphFour />
       <ParagraphSix />
